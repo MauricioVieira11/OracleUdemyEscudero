ae785898ccf2861af0b14ba34ce8464efb1fa4b7
Mauricio <mauriciovieira8@hotmail.com>
2022-04-01 11:34:52 -0300

    Sexto commit curso oracle

---
+++ Seção 29 - Tipos compostos plsql -- variável Record, rowType/Seção+10+-+Prática+Aula+1.sql
@@ -0,0 +1,32 @@
+--
+-- Se√ß√£o 10 - Tipos Compostos - Vari√°vel Tipo PL/SQL Record
+--
+-- Aula 1 - Vari√°vel Tipo PL/SQL Record
+--
+
+-- Criando um PL/SQL Record 
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF --n„o imprime o cÛdigo do bloco
+ACCEPT pemployee_id PROMPT 'Digite o Id do empregado: '
+DECLARE
+TYPE  employee_record_type IS RECORD --
+          (employee_id   employees.employee_id%type,
+           first_name    employees.first_name%type,
+           last_name     employees.last_name%type,
+           email         employees.email%type,
+           phone_number  employees.phone_number%type);
+employee_record  employee_record_type; --vari·vel employee_record com o tipo employee_record_type
+
+BEGIN
+SELECT  employee_id, first_name, last_name, email, phone_number
+INTO    employee_record
+FROM    employees
+WHERE   employee_id = &pemployee_id;
+DBMS_OUTPUT.PUT_LINE(employee_record.employee_id || ' - ' || 
+                     employee_record.first_name || ' - ' || 
+                     employee_record.last_name || ' - ' || 
+                     employee_record.phone_number);
+END;
+
+
